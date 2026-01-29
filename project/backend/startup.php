2e7fe5197f791754ff9456c6566267ea867a41f1
Javad <MrJavad.Email@gmail.com>
2026-01-29 20:41:28 +0330

    fix(main): Fixed startup.php
    - import db.php to use create_table method
    - change PRIMARY KET to PRIMARY KEY

--- project/backend/startup.php
+++ project/backend/startup.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . '/database/init.php';
+require_once __DIR__ . '/database/db.php';
 
 if (file_exists(".started")) {
     exit;
@@ -9,7 +10,7 @@ create_database();
 create_table(
     "users",
     [
-        "id" => "INT AUTO_INCREMENT PRIMARY KET",
+        "id" => "INT AUTO_INCREMENT PRIMARY KEY",
         "username" => "VARCHAR(255) NOT NULL UNIQUE",
         "password" => "VARCHAR(255) NOT NULL"
     ]
