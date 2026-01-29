873e9bcb74aa3a8d3065e31dd8a36fe3e2144a90
Javad <MrJavad.Email@gmail.com>
2026-01-29 19:38:26 +0330

    feat: add startup.php for database initialization

---
+++ project/backend/startup.php
@@ -0,0 +1,18 @@
+<?php
+require_once __DIR__ . '/database/init.php';
+
+if (file_exists(".started")) {
+    exit;
+}
+
+create_database();
+create_table(
+    "users",
+    [
+        "id" => "INT AUTO_INCREMENT PRIMARY KET",
+        "username" => "VARCHAR(255) NOT NULL UNIQUE",
+        "password" => "VARCHAR(255) NOT NULL"
+    ]
+);
+
+file_put_contents(".started", "started: true");
